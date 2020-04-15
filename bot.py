ca8eb632d955e5b13c579657ff70502df78612dd
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-15 22:16:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(func=lambda message:True, content_types=['start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, '( >_<)')
 
