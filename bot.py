199894730eaced65a16140e3205830089d21de6a
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-15 21:53:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ def send_welcome(message):
 	bot.reply_to(message, '( >_<)')
 @bot.message_handler(commands=['text'])
 def send_message(text):
-	bot.message.text('take me 👿')
+	bot.send_message('take me 👿')
 
 #RUN
 bot.polling(none_stop= True)
