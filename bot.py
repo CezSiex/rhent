c518c7d33946e87162976d99ff77b819161feb27
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-15 21:51:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, '( >_<)')
-@bot.command(pass_context = True)
-async def text():
-	await Bot.say('take me 👿')
+@bot.message_handler(commands=['text'])
+def send_message(text):
+	bot.message.text('take me 👿')
 
 #RUN
 bot.polling(none_stop= True)
