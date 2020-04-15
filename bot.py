c34e6375c454bd75bba3c78a8d3f02f29d20b882
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-15 23:09:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
-import telebo
+import telebot
 import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-	bot.reply_to(message, '( >_<)')
+def send_start(message):
+	bot.send_message(message.chat.id, '( >_<)')
 
 @bot.message_handler_1(commands=['text'])
-bot.send_message("take me 👿")
+bot.send_message(' take me ')
 
 #RUN
 bot.polling(none_stop= True)
