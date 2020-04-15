d4603f005b774bb163517f9c66a78dec56fa2072
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-15 23:11:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ def send_start(message):
 	bot.send_message(message.chat.id, '( >_<)')
 
 @bot.message_handler_1(commands=['text'])
-bot.send_message(' take me ')
+def send_text(message):
+	bot.send_message(message.chat.id,' take me ')
 
 #RUN
 bot.polling(none_stop= True)
