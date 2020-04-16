6e924365344ff569fec42306201854f7d45f96a5
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-16 14:34:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	bot.send_document(message.chat.id,'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F38%2Fdb%2F07%2F38db0791549a04e0139e90cbdb8a5f32.gif&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F12455336456000157%2F&tbnid=szxC_FvCbq7FrM&vet=10CB4QxiAoCGoXChMI8Oi6-Ons6AIVAAAAAB0AAAAAEAc..i&docid=daWkArMm12S05M&w=800&h=600&itg=1&q=gif%20%D0%B4%D0%BB%D1%8F%20%D0%B1%D0%BE%D1%82%D0%B0%20%D1%82%D0%B5%D0%BB%D0%B5%D0%B3%D1%80%D0%B0%D0%BC%D0%BC&client=ubuntu&ved=0CB4QxiAoCGoXChMI8Oi6-Ons6AIVAAAAAB0AAAAAEAc')
+	bot.send_document(message.chat.id,'https://github.com/CezSiex/rhent/blob/master/photo/video_2020-04-16_13-53-18.mp4')
 
 @bot.message_handler(commands=['Hey', 'hey', 'HEY', 'HEy', 'hEY', 'heY'])
 def send_start(message):
