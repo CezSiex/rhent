26d91bb9a37943a5de483f862793b56d35087610
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-16 15:00:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	bot.send_document(message.chat.id,'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgifer.com%2Fru%2F9Ptf&psig=AOvVaw3OPM8H9_Ns6iGHPlgGN2pc&ust=1587124778848000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDy4Njy7OgCFQAAAAAdAAAAABAD')
+	bot.send_document(message.chat.id,'https://i.gifer.com/9Ptf.gif')
 
 @bot.message_handler(commands=['Hey', 'hey', 'HEY', 'HEy', 'hEY', 'heY'])
 def send_start(message):
