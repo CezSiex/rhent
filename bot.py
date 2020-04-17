04b059c115dbe9cdde36a587f681444c0e1c6813
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-17 22:33:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,12 @@ bot = telebot.TeleBot(config.TOKEN)
 # Help list 
 @bot.message_handler(commands=['help'])
 def help(message):
-	bot.send_message(message.chat.id, '''This is help list (>_<) -- # let's get started \n1$com -- /hey # HEY \n2$com -- /gif # check gif \n3$com -- /want # see \n4$com -- /photo # see photo Kera's \n5$com -- /help # Why?''')	
+	bot.send_message(message.chat.id, '''This is help list (>_<) -- # let's get started \n1$com -- /hey # HEY \n2$com -- /gif # check gif \n3$com -- /want # see \n4$com -- /photo # see photo Kera's \n5$com -- /help # Why?''')
+		
 # Start
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	bot.send_document(message.chat.id, 'https://media.giphy.com/media/xdgisqRDFyO9G/giphy.gif'  )
+	bot.send_document(message.chat.id, 'I greet --/${name}$/ check -- /help # help list \nhttps://media.giphy.com/media/xdgisqRDFyO9G/giphy.gif \nНou are like a wanderer wandering from neotkudap '  ).format(message.from_user.id)
 
 @bot.message_handler(commands=['Hey', 'hey', 'HEY', 'HEy', 'hEY', 'heY'])
 def send_start(message):
@@ -28,6 +29,8 @@ def send_photo(message):
 @bot.message_handler(commands=['gif'])
 def gif(message):
 	bot.send_document(message.chat.id, 'https://media.giphy.com/media/oVREpe8qWMOqc/giphy.gif'  )	
+
+
 		
 #RUN
-bot.polling(none_stop= True)
+bot.polling(none_stop= True))
