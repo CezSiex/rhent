b8f753f12fb32796be2ff392f6b96a7139282971
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-19 22:38:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def send_photo(message):
 def send_test(message):
 	all_files_in_directory = os.listdir('https://github.com/CezSiex/rhent/tree/master/photo/hPhoto/photo_kerabot')
 	file = random.choice(all_files_in_directory)
-	doc = open('https://github.com/CezSiex/rhent/tree/master/photo/hPhoto/photo_kerabot' + '/' + file, 'rd')
+	doc = open(file)
 	bot.send_photo(chat_id, doc)
 
 
