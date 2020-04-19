09e44d5e4855c321c4de890167edd028928125ca
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-19 13:09:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ def welcome(message):
 	bot.send_document(message.chat.id, 'https://media.giphy.com/media/xdgisqRDFyO9G/giphy.gif')
 	
 	if (True == True):
-		bot.send_message(message.chat.id, 'You are like a wanderer wandering from neotkudap (')
 		time.sleep(20)
+		bot.send_message(message.chat.id, 'You are like a wanderer wandering from neotkudap (')	
 	else:
 		print('Process stop')
 
