36695cca3e65b898bd496cb4ee08ab121365792c
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-19 12:12:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ bot = telebot.TeleBot(config.TOKEN)
 def welcome(message):
 	bot.send_message(message.chat.id, 'I greet you --$ {0.first_name} $-- (>_<)\nMy name is {1.first_name} \ncheck -- /help # help list '.format(message.from_user, bot.get_me()))
 	bot.send_document(message.chat.id, 'https://media.giphy.com/media/xdgisqRDFyO9G/giphy.gif')
-	time.sleep(0.5)
 	bot.send_message(message.chat.id, 'You are like a wanderer wandering from neotkudap (')
 
 
