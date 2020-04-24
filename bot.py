ede623e8533ecc64b999787eca74880d34bf67ca
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-24 20:39:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def send_photo(message):
 # hentphoto
 @bot.message_handler(commands=['hentphoto'])
 def send_test(message): 
-	bot.send_photo(message.chat.id, '<a href="https://github.com/CezSiex/rhent/blob/master/photo/photo_kerabot/photo1.jpg">&#8203;</a>',parse_mode="HTML")     # random.choice(os.listdir('https://github.com/CezSiex/rhent/tree/master/photo/hPhoto/photo_kerabot'))
+	bot.send_photo(message.chat.id, 'https://raw.githubusercontent.com/CezSiex/rhent/master/photo/photo_2020-04-16_10-11-48.jpg')     # random.choice(os.listdir('https://github.com/CezSiex/rhent/tree/master/photo/hPhoto/photo_kerabot'))
 
 
 # Help list 
